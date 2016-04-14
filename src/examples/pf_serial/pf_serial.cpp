da1eff9714604ddd402736ccb329c3ee53ed308b
Juraj Oršulić <juraj5@gmail.com>
2016-04-14 20:12:24 +0200

    pf_serial: add loading distance map from SD card
    - progress on porting (for now) SW particle filter to f32c

--- src/examples/pf_serial/pf_serial.cpp
+++ src/examples/pf_serial/pf_serial.cpp
@@ -1,15 +1,26 @@
+
+
 extern "C" {
     #include <stdio.h>
-    #include <io.h>
+    #include <stdlib.h>
+    #include <dev/io.h>
     #include <mips/io.h>
+    #include <dev/sio.h>
     #include <mips/_fpmath.h>
     #include <stdarg.h>
     #include <string.h>
+    #include <fcntl.h>    
+    #include <unistd.h>
 }
 
-#include "/home/juraj/projects/catkin/mr2015ws/src/tuw_mr2015/tuw_self_localization/include/tuw_self_localization/fixedpoint.h"
+#include <fatfs/ff.h>
+    
+    
+#include "tuw_mr2015/tuw_self_localization/include/tuw_self_localization/fixedpoint.h"
 
 
+////////////////
+// debouncing for button inputs
 static unsigned int _tprev = 0;
 inline void eventPress() {
     
@@ -35,11 +46,12 @@ inline void eventdePress() {
     _tprev = t;
 }
 
+////////////////
 
-extern char sio_rxbuf[];
-extern uint8_t sio_rxbuf_tail;
 
-void sync() {
+// waits for "sync" on serial, checks if RX buffer is clear 
+// to ensure synchronisation, answers with "sack".
+bool sync_serial() {
     // sync
     int sync = 0;
     //printf("Beginning sync...\n");
@@ -50,85 +62,145 @@ void sync() {
         //printf("Received: %x \n", p);
         
         if(p==synctoken[sync]) sync++;
-        else sync = 0;
-        
-        //printf("Sync = %d\n", sync);
+        else 
+            return false;
         
     }
+    if (!sio_rxempty()) 
+        return false;
+    
     printf("sack");
+    return true;
     
 }
 
-//template<class tip> int asint(tip a) { return int(a); }
+
+#include "tuw_mr2015/tuw_self_localization/include/tuw_self_localization/com_structs.h"
+
+
+// helper functions for printing various data types 
 void print(fixed a) { printf("%f", float(a)); }
 void print(int a) { printf("%d", a); }
+void print(unsigned int a) { printf("%u", a); }
 void print(bool a) { printf("%d", a); }
+void print(likelihoodLookuptable a) {
+        printf("[");
+        for(int i = 0; i<5; i++)
+            printf("%.5f ", float(a.gausspdf[i]));
+        printf("...]");
+}
+
+
+// generic function for reading a struct via serial link from master
+template<class msg> void readstruct(msg& m) {
+    unsigned char *p = (unsigned char *) &m;
+    for(unsigned int i = 0; i<sizeof(m); i++) {
+        *p = getchar(); p++;
+    }
+
+}
+
+// distance-transformed map is stored in heap 
+unsigned char *map_mem;
+unsigned int map_size;
+// TODO: making map size dynamic.
+unsigned int mapx = 558; unsigned int mapy = 558;
+
+
+// 2D array-like function for accessing map value at pixel coordinates c,r
+unsigned char& map(int c, int r) {
+    return *(map_mem + r + c * mapx);    
+}
+
+
+msgtype_sample *samples_mem;
+unsigned int allocated_samples = 0;
 
 
 void main(void)
 {
     
+    sio_setbaud(1152000);
+    int f = open("d:likelihood.map", O_RDONLY);
+    map_size = lseek(f, 0, SEEK_END);
+        
+    map_mem = (unsigned char*) malloc(map_size);
+    
+    if(map_mem == NULL)  {
+        printf("Could not allocate memory for map!\n");
+        return;
+    }
+        
+    
+    lseek(f, 0, SEEK_SET);
+    
+    read(f, map_mem, map_size);
+    
+
+    printf("Read likelihood.map, handle %d, size %u\n", f, map_size);
+    
+    // TODO: read map dimensions from file
+           
+    // 2D array pointer
+    
+    //for(int i = 0; i < 20; i++)
+    //    printf("%x ", map(0,i));
+    
+    //printf("\n");
+    
     while(1) {
         
-        sync();
-    
+        while(!sync_serial()) { };
+        
         char ctmp[5];
         for(int i = 0; i < 4; i++)
             ctmp[i] = getchar();
         
         ctmp[4] = '\0';
-              
-        if(strcmp(ctmp, "pars")) {
-            printf("\nSomething wrong, expected pars, got %s!\n",ctmp);
-            continue; }
-        else printf("\nReceiving pars on FPGA...\n");
         
-        struct {
-            bool enable_resample;
-            bool enable_weighting;
-            bool enable_update;
-            fixed alpha1;
-            fixed alpha2;
-            fixed alpha3;
-            fixed alpha4;
-            fixed alpha5;
-            fixed alpha6;
-            int nr_of_samples;
-            int nr_of_beams;
-        } pars;
-        
-        
-        unsigned char *p = (unsigned char *) &pars;
-        for(unsigned int i = 0; i<sizeof(pars); i++) {
-            *p = getchar();
-            printf("Received byte %d/%d: %x\n", i+1, sizeof(pars), *p);
-            p++;
-        }
+        if(strcmp(ctmp, "pars") == 0) {
+            printf("Receiving pars on FPGA...\n");
             
+            readstruct(msg_params);
+            
+            printf("Received pars:\n");
+            
+            #define member(t,x,y) printf(#t " " #x " = "); print(msg_params.x); printf("\n")
+            com_params;
+            #undef member
+            
+            if(msg_params.nr_of_samples != allocated_samples) {
+                samples_mem = (msgtype_sample*) realloc(samples_mem, sizeof(msgtype_sample) * msg_params.nr_of_samples);
+                if(samples_mem == NULL)  {
+                    printf("Could not allocate memory for %d samples!\n", msg_params.nr_of_samples);
+                    return;
+                }
+                else printf("Allocated %d samples.\n", msg_params.nr_of_samples);
+            }
+                
+                printf("!end!\n");
+            }
 
+        else if(strcmp(ctmp, "data") == 0) {
+            
+            printf("Receiving data on FPGA...\n");
+            
+            readstruct(msg_frame_data);
+            
+            printf("Received data:\n");
+            
+            #define member(t,x,y) printf(#t " " #x " = "); print(msg_frame_data.x); printf("\n")
+            com_frame_data;
+            #undef member
+            
+            printf("!end!\n");
+            
+        }  
         
-        printf("Received data:\n");
-       
-        #define setparam(x) printf(#x); printf(" = "); print(pars.x); printf("\n")
+        else printf("\nSomething wrong, expected a message id, got %s!\n",ctmp);
+        continue; }
         
-        setparam(enable_resample);
-        setparam(enable_weighting);
-        setparam(enable_update);
-        setparam(alpha1);
-        setparam(alpha2);
-        setparam(alpha3);
-        setparam(alpha4);
-        setparam(alpha5);
-        setparam(alpha6);
-        setparam(nr_of_samples);
-        setparam(nr_of_beams);
         
-        #undef setparam        
-        
-                
-        printf("!end!\n");
-                
-        
-    }
-    
 }
+
+
