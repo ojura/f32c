a80a1802054a3f62fb98ea94602c56c34b115f20
Juraj Oršulić <juraj5@gmail.com>
2016-05-06 15:26:57 +0200

    pf_serial: 40 particles, slightly more position noise

--- src/examples/pf_serial/run_pf.sh
+++ src/examples/pf_serial/run_pf.sh
@@ -18,7 +18,7 @@ source /opt/ros/kinetic/setup.bash
 source devel/setup.bash
 
 export MR_DIR=$DIR/mr2015ws/src/tuw_mr2015
-rosrun tuw_self_localization tuw_self_localization_node cmd:=cmd_vel scan:=base_scan _plot_data:=true _mode:=0 _initial_with_ground_truth:=true _plot_data:=true _reinitialize:=false _particle_filter/initial_distribution:=1 _particle_filter/forward_prediction_time:=0 _particle_filter/enable_update:=true _particle_filter/enable_resample:=true _particle_filter/enable_weighting:=true _particle_filter/sigma_init_position:=0.2 _particle_filter/sigma_init_orientation:=0.1 _particle_filter/sigma_static_position:=0.05 _particle_filter/sigma_static_orientation:=0.1 _particle_filter/initial_distribution:=0 _particle_filter/resample_strategy:=0 _map_image:="${MR_DIR}/tuw_self_localization/maps/cave.png" _map_lines:="${MR_DIR}/tuw_self_localization/maps/cave.yml" _particle_filter/nr_of_samples:=20 &
+rosrun tuw_self_localization tuw_self_localization_node cmd:=cmd_vel scan:=base_scan _plot_data:=true _mode:=0 _initial_with_ground_truth:=true _plot_data:=true _reinitialize:=false _particle_filter/initial_distribution:=1 _particle_filter/forward_prediction_time:=0 _particle_filter/enable_update:=true _particle_filter/enable_resample:=true _particle_filter/enable_weighting:=true _particle_filter/sigma_init_position:=0.2 _particle_filter/sigma_init_orientation:=0.1 _particle_filter/sigma_static_position:=0.1 _particle_filter/sigma_static_orientation:=0.1 _particle_filter/initial_distribution:=0 _particle_filter/resample_strategy:=0 _map_image:="${MR_DIR}/tuw_self_localization/maps/cave.png" _map_lines:="${MR_DIR}/tuw_self_localization/maps/cave.yml" _particle_filter/nr_of_samples:=40 &
 
 function ctrl_c() {
     killall roslaunch
